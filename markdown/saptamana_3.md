39ed0ba0063854674d1a2f6627b4f6d2660076cf
Cătălin Pleșu <catalinhimself@gmail.com>
2022-03-11 20:41:02 +0200

    saptamana 3 finisata

---
+++ markdown/saptamana_3.md
@@ -0,0 +1,10 @@
+Sarcini planificate:
+I. Crearea unui scene simple si realizarea controlului camerei.
+
+Activităţi desfăşurate, observaţii personale:
+In saptamana aceasta am utilizat frameworkul bevy petnru a crea un plan, si
+am adaugat o camera pentru a il putea vuzializa plus un cub.
+Apoi am cercetat si am descoperit ca BEVY nu ofera un motor de fizica astfel 
+in urma unor cautari chiar pe web siteul bevyengine.org am descoperit motorul
+de fizica rapier care are posibilitati de fizica 3d si 2d. Apoi am adaugat 
+fizica planului si cubuilui.
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,170 @@
-use bevy::prelude::*;
+use bevy::{
+    app::{App, CoreStage, Plugin},
+    pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
+    prelude::*,
+    DefaultPlugins,
+};
+use bevy_rapier3d::{
+    physics::wrapper::{ColliderMassPropsComponent, ColliderMaterialComponent},
+    prelude::{
+        ColliderBundle, ColliderMassProps, ColliderMaterial, ColliderPositionSync, ColliderShape,
+        Isometry, NoUserData, Point, RapierPhysicsPlugin, Real, RigidBodyBundle, RigidBodyPosition,
+        RigidBodyType,
+    },
+    render::RapierRenderPlugin,
+};
+use core::f32::consts::PI;
 
-pub struct PrototypeGamePlugin;
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierRenderPlugin)
+        .add_plugin(WireframePlugin)
+        .add_plugin(CameraPlugin)
+        .add_plugin(LightPlugin)
+        .add_plugin(WorldPlugin)
+        .add_plugin(CubePlugin)
+        .run();
+}
 
-impl Plugin for PrototypeGamePlugin {
+pub struct CameraPlugin;
+impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(print_player);
+        app.add_startup_system(generate_camera);
     }
 }
 
-fn spawn_player(mut commands: Commands) {
-    commands.spawn().insert_bundle(PlayerBundle {
-        name: Name("Catalin".to_string()),
+fn generate_camera(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 }
 
-fn print_player(query: Query<&Name ,With<Player>>) {
-    for name in query.iter() {
-        println!("player {}", name.0);
+pub struct CubePlugin;
+impl Plugin for CubePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(generate_cube);
     }
 }
 
-#[derive(Default, Component)]
-struct NPC;
-
-#[derive(Default, Component)]
-struct Player;
-
-#[derive(Default, Component)]
-struct Name(String);
-
-#[derive(Default, Component)]
-struct Vitals {
-    health: f32,
-    stamina: f32,
+fn generate_cube(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 2.0, 0.0),
+            ..Default::default()
+        })
+        .insert_bundle(RigidBodyBundle {
+            body_type: RigidBodyType::Dynamic.into(),
+            ..Default::default()
+        })
+        .insert_bundle(ColliderBundle {
+            shape: ColliderShape::cuboid(0.5, 0.5, 0.5).into(),
+            mass_properties: ColliderMassPropsComponent(ColliderMassProps::Density(1.)),
+            ..Default::default()
+        })
+        .insert(ColliderPositionSync::Discrete)
+        .insert(Wireframe);
 }
 
-#[derive(Default, Component)]
-struct Attributes {
-    strength: f32,
+pub struct LightPlugin;
+impl Plugin for LightPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(generate_light);
+    }
 }
 
-#[derive(Default, Component)]
-struct Position {
-    x: f32,
-    y: f32,
-}
-#[derive(Default, Component)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Bundle, Default)]
-struct PlayerBundle {
-    player: Player,
-    name: Name,
-    vitals: Vitals,
-    attributes: Attributes,
-    position: Position,
-    velocity: Velocity,
+fn generate_light(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(PrototypeGamePlugin)
-        .run();
+pub struct WorldPlugin;
+impl Plugin for WorldPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(generate_plane);
+    }
+}
+
+fn generate_plane(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let plane_half = 50.0;
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane {
+                size: plane_half * 2.0,
+            })),
+            material: materials.add(Color::rgba(0.2, 0.6, 0.2, 0.5).into()),
+            ..Default::default()
+        })
+        .insert_bundle(RigidBodyBundle {
+            body_type: RigidBodyType::Static.into(),
+            ..Default::default()
+        })
+        .insert_bundle(ColliderBundle {
+            shape: ColliderShape::cuboid(plane_half, 0., plane_half).into(),
+            // mass_properties: ColliderMassPropsComponent(MassProperties::new(
+            //     Vec3::new(0.0, -0.4, 0.0).into(),
+            //     15.0,
+            //     Vec3::new(100.0, 100.0, 100.0).into(),
+            // )),
+            // material: ColliderMaterialComponent(ColliderMaterial {
+            //     friction: 1.0,
+            //     restitution: 1_000_0000.0,
+            //     ..Default::default()
+            // }),
+            ..Default::default()
+        });
+
+    // for x in -1..=1 {
+    //     for z in -1..=1 {
+    //         commands
+    //             .spawn_bundle(PbrBundle {
+    //                 mesh: meshes.add(Mesh::from(shape::Plane { size: 1.0 })),
+    //                 material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //                 transform: Transform::from_xyz(x as f32, 0., z as f32),
+    //                 ..Default::default()
+    //             })
+    //             .insert_bundle(RigidBodyBundle {
+    //                 body_type: RigidBodyType::Static.into(),
+    //                 ..Default::default()
+    //             })
+    //             .insert_bundle(ColliderBundle {
+    //                 shape: ColliderShape::cuboid(1., 1., 1.).into(),
+    //                 // material: ColliderMaterialComponent ( ColliderMaterial{
+    //                 //     friction: 1.0,
+    //                 //     restitution: 1_000_0000.0,
+    //                 //     ..Default::default()
+    //                 // }),
+    //                 ..Default::default()
+    //             })
+    //             .insert(Wireframe);
+    //     }
+    // }
 }
